c3feafe5f25e2544deabe352dd9dc06ac3d5ea3c
Milena Mota <80923819+eummilena@users.noreply.github.com>
2025-10-31 15:18:59 -0300

    feat: componente criado

--- move/src/App.tsx
+++ move/src/App.tsx
@@ -1,14 +1,14 @@
 import 'react'
-
 import './App.css'
+import Header from './components/Header'
 
 function App() {
 
 
   return (
-    <>
-
-    </>
+    <div>
+      <Header />
+    </div>
   )
 }
 
