382efe374097aa3eae7e3b21eafbe342be566a0c
Milena Mota <80923819+eummilena@users.noreply.github.com>
2025-11-10 15:21:46 -0300

    feat: nova section

---
+++ move/src/pages/Move/Move.tsx
@@ -0,0 +1,63 @@
+import Button from '../../components/Button'
+import styles from './Move.module.css'
+import home from '../../assets/images/house.svg'
+import truck from '../../assets/images/cargo-truck.svg'
+import boxes from '../../assets/images/stack-package.svg'
+import immediate from '../../assets/images/clock.svg'
+
+
+
+const steps = [
+    {
+        id: 1,
+        img: home,
+        step: "1. Insira seus detalhes de movimentação.",
+        desc: "Adicione os detalhes da mudança e obtenha o preço pela mudança imediatamente.",
+    },
+    {
+        id: 2,
+        img: boxes,
+        step: "2. Organize tudo em um só lugar.",
+        desc: "Vamos ajudá-lo a criar uma lista de coisas para mover. Vamos dominar isso juntos.",
+    },
+    {
+        id: 3,
+        img: immediate,
+        step: "3. Dia de mudança. Sem pressa.",
+        desc: "No grande dia, nossos carregadores chegam e fazem todo o trabalho duro.",
+    },
+    {
+        id: 4,
+        img: truck,
+        step: "4. Pague somente após a mudança.",
+        desc: "Pague somente após nossos funcionários concluírem toda mudança de seus pertences em sua nova.",
+    }
+
+]
+
+const Move = () => {
+    return (
+        <section className={styles.section}>
+            <h2>Como funciona?</h2>
+            <p>A move simplifica todo o processo de movimentação</p>
+            <div className={styles.info} >
+                <ul>
+                    {steps.map(({ step, img, desc, id }) => (
+                        <li key={id} className={styles.steps}>
+                            <span className={styles.span}>
+                                <img src={img} alt='' className={styles.img} />
+                            </span>
+                            <div className={styles.step}>
+                                <h5>{step}</h5>
+                                <p>{desc}</p>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <Button>Faça um orçamento online</Button>
+        </section>
+    )
+}
+
+export default Move
